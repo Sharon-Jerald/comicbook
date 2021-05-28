1b430015b91f87d991d0f1c33f7e8e671c25ec7c
Sharon-Jerald <sharuz9799@gmail.com>
2021-05-28 15:48:05 +0530

    comic book

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,7 +51,7 @@
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto " href="/login">Login</a></li>
           <li><a class="nav-link scrollto " href="/register">Register</a></li>
-          <li><a class="nav-link scrollto " href="/adminregister">Admin Register</a></li>
+      
 
           
         </ul>
